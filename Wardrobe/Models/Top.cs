901d6728503019343896bf4334d4f07ce853bfba
Luis Cabrera <loudigi@gmail.com>
2017-05-01 02:45:34 -0400

    All styles set, all views ready.

--- Wardrobe/Models/Top.cs
+++ Wardrobe/Models/Top.cs
@@ -11,7 +11,8 @@ namespace Wardrobe.Models
 {
     using System;
     using System.Collections.Generic;
-    
+ 
+
     public partial class Top
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
